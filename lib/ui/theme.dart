8c090f234844860f8995faf53072ad7d66191e3c
dolon <dolonk9@gmail.com>
2023-05-08 11:24:34 +0600

    Theme Customization

---
+++ lib/ui/theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+const Color bluishClr = Color(0xff4e5ae8);
+const Color yellowClr = Color(0xffFFB746);
+const Color pinkClr = Color(0xffff4667);
+const Color whiteClr = Colors.white;
+const primaryClr = bluishClr;
+const Color darkGreyClr = Color(0xff121212);
+const Color darkHeaderClr = Color(0xff424242);
+
+
+class Themes {
+
+  static final light = ThemeData(
+      primaryColor: primaryClr,
+      brightness: Brightness.light
+  );
+
+  static final dark = ThemeData(
+      primaryColor: darkGreyClr,
+      brightness: Brightness.dark
+  );
+}
