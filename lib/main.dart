9ad212a78856c53f3157f904ebb987212333ebc7
dolon <dolonk9@gmail.com>
2023-05-09 11:07:25 +0600

    Notification Set

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,24 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:task_mnagment/services/notification_services.dart';
+
 import 'package:task_mnagment/services/theme_services.dart';
 import 'package:task_mnagment/ui/home_page.dart';
 import 'package:task_mnagment/ui/theme.dart';
 
+
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
+  NotifyHelper().notificationSettings();
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -25,4 +34,7 @@ class MyApp extends StatelessWidget {
         themeMode: ThemeServices().theme,
         home: const HomePage());
   }
+
 }
+
+
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:task_mnagment/services/theme_services.dart';
+import '../services/notification_services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,32 +11,48 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var notifyHelper;
+
+  @override
+  void initState() {
+    super.initState();
+    notifyHelper = NotifyHelper();
+    notifyHelper.checkForNotification();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appBar(),
-      body: Column(
-        children: [
-          Text(
-            'Theme Data',
-            style: TextStyle(fontSize: 30),
-          )
-        ],
-      ),
+      body: Center(
+          child: ElevatedButton(
+        child: const Text('Show notifications'),
+        onPressed: () {},
+      )),
     );
   }
 
   _appBar() {
     return AppBar(
       leading: GestureDetector(
-        onTap: (){
+        onTap: () {
           ThemeServices().switchTheme();
+          // NotifyHelper().showNotification();
+          notifyHelper.showNotification();
         },
-        child: Icon(Icons.nightlight_rounded,size: 20,),
+        child: Icon(
+          Icons.nightlight_rounded,
+          size: 20,
+        ),
       ),
       actions: [
-        Icon(Icons.person,size: 20,),
-        SizedBox(width: 20,)
+        Icon(
+          Icons.person,
+          size: 20,
+        ),
+        SizedBox(
+          width: 20,
+        )
       ],
     );
   }
