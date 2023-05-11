5358d499d7d7785dc8701a500383ab88d6eefc67
dolon <dolonk9@gmail.com>
2023-05-11 14:00:43 +0600

    Created bottom sheet dialog to dynamic way

--- lib/ui/home_page/home_page.dart
+++ lib/ui/home_page/home_page.dart
@@ -2,6 +2,7 @@ import 'package:date_picker_timeline/date_picker_timeline.dart';
 import 'package:date_time_picker/date_time_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -10,6 +11,8 @@ import 'package:task_mnagment/controllers/task_controller.dart';
 import 'package:task_mnagment/services/theme_services.dart';
 import 'package:task_mnagment/ui/theme.dart';
 import 'package:task_mnagment/ui/widget/button.dart';
+import 'package:task_mnagment/ui/widget/task_tile.dart';
+import '../../models/task_model.dart';
 import '../../services/notification_services.dart';
 import '../add_task_page/add_task_page.dart';
 
@@ -110,7 +113,7 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  _appTaskBar(){
+  _appTaskBar() {
     return Container(
       margin: const EdgeInsets.only(left: 15, right: 15, top: 15),
       child: Row(
@@ -131,7 +134,7 @@ class _HomePageState extends State<HomePage> {
           ),
           MyButton(
               label: "+ Add Task",
-              onTap: ()  async {
+              onTap: () async {
                 await Get.to(() => AddTaskPage());
                 _taskController.getTasks();
               })
@@ -141,23 +144,139 @@ class _HomePageState extends State<HomePage> {
   }
 
   _showTasks() {
-    return Expanded(
-        child: Obx(() {
+    return Expanded(child: Obx(() {
       return ListView.builder(
         itemCount: _taskController.taskList.length,
-          itemBuilder: (context, index) {
+        itemBuilder: (context, index) {
           print(_taskController.taskList.length);
-            return Container(
-              width: 100,
-              height: 50,
-              color: Colors.green,
-              margin: EdgeInsets.only(bottom: 10),
-              child: Text(
-                  _taskController.taskList[index].title.toString()
-              ),
-            );
-          },);
+          return AnimationConfiguration.staggeredList(
+              position: index,
+              child: SlideAnimation(
+                child: Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        _showBottomSheet(
+                            context, _taskController.taskList[index]);
+                      },
+                      child: TaskTile(_taskController.taskList[index]),
+                    )
+                  ],
+                ),
+              ));
+        },
+      );
     }));
   }
 
+  _showBottomSheet(BuildContext context, Task task) {
+    Get.bottomSheet(
+      Container(
+        padding: const EdgeInsets.only(top: 4),
+        height: task.isCompleted == 1
+            ? MediaQuery.of(context).size.height * 0.24
+            : MediaQuery.of(context).size.height * 0.32,
+        color: Get.isDarkMode ? darkGreyClr : Colors.white,
+        child: Column(
+          children: [
+            Container(
+              height: 6,
+              width: 120,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Get.isDarkMode ? Colors.grey[600] : pinkClr),
+            ),
+            Spacer(),
+            task.isCompleted == 1
+                ? Container()
+                : _bottomSheetButton(
+                    label: "Task Completed",
+                    onTap: () {
+                      Get.back();
+                    },
+                    clr: primaryClr,
+                    context: context),
+            SizedBox(height: 5),
+            _bottomSheetButton(
+                label: " Delete Task",
+                onTap: () {
+                  Get.back();
+                },
+                clr: Colors.redAccent,
+                context: context),
+            SizedBox(height: 20),
+            _bottomSheetButton(
+                label: " Close",
+                onTap: () {
+                  Get.back();
+                },
+                clr: Colors.redAccent,
+                isClose: true,
+                context: context),
+            SizedBox(height: 10)
+          ],
+        ),
+      ),
+    );
+  }
+
+  _bottomSheetButton(
+      {required String label,
+      required Function()? onTap,
+      required Color clr,
+      required BuildContext context,
+      bool isClose = false}) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 4),
+        height: 55,
+        width: MediaQuery.of(context).size.width * 0.9,
+        decoration: BoxDecoration(
+          border: Border.all(
+              width: 2,
+              color: isClose == true
+                  ? Get.isDarkMode
+                      ? Colors.grey[600]!
+                      : Colors.grey[300]!
+                  : clr),
+          borderRadius: BorderRadius.circular(20),
+          color: isClose == true ? Colors.transparent : clr,
+        ),
+        child: Center(
+          child: Text(
+            label,
+            style:
+                isClose ? titleStyle : titleStyle.copyWith(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
 }
+
+// Column(
+//   children: [
+//     const SizedBox(height: 20),
+//     const Center(
+//       child: Text(
+//         'Bottom Sheet',
+//         style: TextStyle(fontSize: 18),
+//       ),
+//     ),
+//     OutlinedButton(
+//       onPressed: () {
+//         Get.back();
+//       },
+//       child: const Text('Close'),
+//     ),
+//   ],
+// ),
+// backgroundColor: Colors.white,
+// elevation: 0,
+// shape: const RoundedRectangleBorder(
+//   borderRadius: BorderRadius.only(
+//     topLeft: Radius.circular(25.0),
+//     topRight: Radius.circular(25.0)
+//   ),
+// ),
